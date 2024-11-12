8946aa58da714b04077fbe9ba12a3f7f5c222da9
O.T. <29921+oleksify@users.noreply.github.com>
2024-11-12 17:48:53 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -46,7 +46,7 @@ ensure_bucket_exists() {
 }
 
 pg_dump_database() {
-    pg_dump  --no-owner --no-privileges --clean --if-exists --quote-all-identifiers "$DATABASE_URL"
+    pg_dump --jobs 1 --no-owner --no-privileges --clean --if-exists --quote-all-identifiers "$DATABASE_URL"
 }
 
 upload_to_bucket() {
