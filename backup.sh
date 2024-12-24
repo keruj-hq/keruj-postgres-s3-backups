1ecd528cb811fdd768c5a546632fc2490e7473a9
O.T. <29921+oleksify@users.noreply.github.com>
2024-12-24 20:06:24 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -12,39 +12,6 @@ s3api() {
     aws s3api "$1" --endpoint "$AWS_ENDPOINT_URL_S3" --bucket "$S3_BUCKET_NAME" "${@:2}"
 }
 
-bucket_exists() {
-    s3 ls "$S3_BUCKET_NAME" &> /dev/null
-}
-
-create_bucket() {
-    echo "Bucket $S3_BUCKET_NAME doesn't exist. Creating it now..."
-
-    # create bucket
-    s3api create-bucket \
-        --create-bucket-configuration LocationConstraint="$AWS_REGION" \
-        --object-ownership BucketOwnerEnforced
-
-    # block public access
-    s3api put-public-access-block \
-        --public-access-block-configuration \
-        "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
-
-    # enable versioning for objects in the bucket 
-    s3api put-bucket-versioning --versioning-configuration Status=Enabled
-
-    # encrypt objects in the bucket
-    s3api put-bucket-encryption \
-      --server-side-encryption-configuration \
-      '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
-}
-
-ensure_bucket_exists() {
-    if bucket_exists; then
-        return
-    fi    
-    create_bucket
-}
-
 pg_dump_database() {
     pg_dump --jobs=1 --no-owner --no-privileges --clean --if-exists --quote-all-identifiers "$DATABASE_URL"
 }
@@ -56,8 +23,6 @@ upload_to_bucket() {
 }
 
 main() {
-    ensure_bucket_exists
-    echo "Taking backup and uploading it to S3..."
     pg_dump_database | gzip | upload_to_bucket
     echo "Done."
 }
